2df0c21aae62c929329d663f7303f84cd9fd64aa
Kiwi <gKiwi.dev@gmail.com>
2025-04-07 13:58:02 +0200

    Create algorytm_euklidesa_dzielenie.py

---
+++ 2T/Algorytmy/algorytm_euklidesa_dzielenie.py
@@ -0,0 +1,8 @@
+liczba1 = int(input("podaj liczbe: "))
+liczba2 = int(input("podaj liczbe: "))
+reszta = 1 
+while reszta != 0:
+    reszta = liczba1 % liczba2
+    liczba1 = liczba2
+    liczba2 = reszta
+print(liczba1)
