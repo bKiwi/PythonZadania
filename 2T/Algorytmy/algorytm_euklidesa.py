1f514fdcb61f3f76bbfd0ab5b9a94dfdc1d3f6d7
Kiwi <gKiwi.dev@gmail.com>
2025-04-07 13:40:02 +0200

    Update algorytm_euklidesa.py

--- 2T/Algorytmy/algorytm_euklidesa.py
+++ 2T/Algorytmy/algorytm_euklidesa.py
@@ -1,1 +1,10 @@
+liczba1 = int(input("podaj liczbe: "))
+liczba2 = int(input("podaj liczbe: "))
 
+while liczba1 != liczba2:
+    if liczba1 > liczba2:
+        liczba1 -= liczba2
+    else:
+        liczba2 -= liczba1
+        
+print(liczba1)    
